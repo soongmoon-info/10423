17997db58a3668463021d14c4eca90f69f618cec
judunge <84211507+judunge@users.noreply.github.com>
2024-09-23 11:49:28 +0900

    Add files via upload

---
+++ pycoding03/learning01.py
@@ -0,0 +1,12 @@
+tempList = [ 10, 20, 30, 40]
+print(tempList)
+
+tempList.append(50)
+print(tempList)
+tempList.insert(2, 25)
+print(tempList)
+
+tempList.remove(50)
+print(tempList)
+del(tempList[2])
+print(tempList)
